46866672cf8bb47f5051d7fa36f142de47a43ff9
Sergio Tovar <sergiotovar13@gmail.com>
2020-07-07 22:21:41 -0500

    Codigo desde create-react-app

---
+++ scorts-gram/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './gifback.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Y aquí empieza nuestra empresa. 
+        </p>
+        <p>Pantallazo a éste hola mundo!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
